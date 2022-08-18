fca9641ace5d4665ae99cc9156a5da4dbf8a05ba
dendense <denyfarras@gmail.com>
2022-08-18 15:30:14 +0700

    Adding Array Map to the Posts Component

--- components/Posts.js
+++ components/Posts.js
@@ -1,59 +1,36 @@
 import * as React from "react";
 import { Box, Image } from "@chakra-ui/react";
 import { Link, Button } from "@chakra-ui/react";
-import DataPosts from "../json/data.json";
+import myData from "../json/data.json";
 
 export default function Posts() {
+  const data = myData.showcases;
+  console.log(data);
   return (
     <Box display={{ md: "flex" }} w="100%" gap="2">
-      <Box
-        id={DataPosts.showcases.id}
-        w="100%"
-        mt={{ base: 4, md: 2 }}
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-      >
-        <Image
-          src={DataPosts.showcases.imageUrl}
-          alt={DataPosts.showcases.title}
-        />
-        <Link href={DataPosts.showcases.siteUrl} isExternal>
-          <Button
-            borderRadius="0"
-            colorScheme="blue"
-            fontWeight="semibold"
-            p="3"
-            w="100%"
-          >
-            {DataPosts.showcases.title}
-          </Button>
-        </Link>
-      </Box>
-      <Box
-        id={DataPosts.showcases.id}
-        w="100%"
-        mt={{ base: 4, md: 2 }}
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-      >
-        <Image
-          src={DataPosts.showcases.imageUrl}
-          alt={DataPosts.showcases.title}
-        />
-        <Link href={DataPosts.showcases.siteUrl2} isExternal>
-          <Button
-            borderRadius="0"
-            colorScheme="gray"
-            fontWeight="semibold"
-            p="3"
-            w="100%"
-          >
-            Underconstruction
-          </Button>
-        </Link>
-      </Box>
+      {data.map((i) => (
+        <Box
+          id={i.id}
+          w="100%"
+          mt={{ base: 4, md: 2 }}
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Image src={i.imageUrl} alt={i.title} />
+          <Link href={i.siteUrl} isExternal>
+            <Button
+              borderRadius="0"
+              colorScheme="blue"
+              fontWeight="semibold"
+              p="3"
+              w="100%"
+            >
+              {i.title}
+            </Button>
+          </Link>
+        </Box>
+      ))}
     </Box>
   );
 }
